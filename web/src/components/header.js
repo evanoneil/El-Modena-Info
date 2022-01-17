b7a601192d008fe3b23a5776eda01ba9a4257521
Evan O'Neil <evan@evanoneil.net>
2022-01-17 12:05:46 -0600

    Update header.js

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -38,7 +38,7 @@ const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="#">
+            <a href="https://elmodena.info/">
               <span className="sr-only">El Modena</span>
               <img
                 className="h-8 w-auto sm:h-10"
